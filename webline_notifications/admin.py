930305ec8d04b06c9e9fbb1f9376530823f4284d
pk karki <pkkarki18@gmail.com>
2019-05-03 16:31:51 +0545

    relationship maintain

--- webline_notifications/admin.py
+++ webline_notifications/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from webline_notifications import Notification
+from webline_notifications.models import Notification
 
 
 class NotificationAdmin(admin.ModelAdmin):
